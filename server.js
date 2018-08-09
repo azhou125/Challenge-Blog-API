0df1de59387763c8558a3796d8dd0a8a370eb272
Ang Zhou <andyza070923@gmail.com>
2018-08-09 12:14:04 -0400

    db-added

--- server.js
+++ server.js
@@ -1,62 +1,181 @@
+"use strict";
 
 const express = require('express');
-const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const morgan = require('morgan'); // for test
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require("./config");
+const { Blog } = require("./models");
 
 const app = express();
 app.use(express.json());
 
-const blogPostsRouter = require('./blogPostsRouter');
-
 // log the http layer
-app.use(morgan('common'));
-
+app.use(morgan('common')); // for test
 app.use(express.static('public'));
 
+// the main page
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.use('/blog-posts', blogPostsRouter);
+//--app.use('/blog-posts', blogPostsRouter);
 
+// GET requests to /blog-posts => return 10 blogs
+app.get("/blog-posts", (req, res) => {
+  const filters = {};
+    const queryableFields = ['title', 'content','author','created'];
+    queryableFields.forEach(field => {
+        if (req.query[field]) {
+            filters[field] = req.query[field];
+        }
+    });
+    Blog
+        .find(filters).limit(10)
+        .then(Blogs => res.json(
+            Blogs.map(blog => blog.serializeWithoutID())
+        ))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({message: 'Internal server error'})
+        });
+});
+
+// can also request by ID
+app.get("/blog-posts/:id", (req, res) => {
+  Blog
+    // this is a convenience method Mongoose provides for searching
+    // by the object _id property
+    .findById(req.params.id)
+    .then(blog => res.json(blog.serializeWithID()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
+
+app.post("/blog-posts", (req, res) => {
+  const requiredFields = ['title', 'content','author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  Blog.create({
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate || Date.now()
+  })
+    .then(blog => res.status(201).json(blog.serializeOrigin()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
+
+app.put("/blog-posts/:id", (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message =
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
+
+  // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const updateableFields = ["title", "content", "author"];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Blog
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .then(blog => res.status(200).json(blog.serializeUpdate()))
+    .catch(err => res.status(500).json({ message: "Internal server error" }));
+});
+
+app.delete("/blog-posts/:id", (req, res) => {
+  Blog.findByIdAndRemove(req.params.id)
+    .then(blog => res.status(204).end())
+    .catch(err => res.status(500).json({ message: "Internal server error" }));
+});
+
+// catch-all endpoint if client makes request to non-existent endpoint
+app.use("*", function(req, res) {
+  res.status(404).json({ message: "Not Found" });
+});
+
+
+
+
+
+
+
+
+
+
+
+// runServer and closeServer part
 let server;
-
-function runServer() {
-  const port = process.env.PORT || 8080;
+// this function connects to our database, then starts the server
+function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    server = app
-      .listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve(server);
-      })
-      .on("error", err => {
-        reject(err);
-      });
+    mongoose.connect(
+      databaseUrl,
+      err => {
+        if (err) {
+          return reject(err);
+        }
+        server = app
+          .listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve();
+          })
+          .on("error", err => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      }
+    );
   });
 }
 
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log("Closing server");
-    server.close(err => {
-      if (err) {
-        reject(err);
-        // so we don't also call `resolve()`
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log("Closing server");
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
   });
 }
 
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
 module.exports = { app, runServer, closeServer };
-
-
-
-
-// app.listen(process.env.PORT || 8080, () => {
-//   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-// });
